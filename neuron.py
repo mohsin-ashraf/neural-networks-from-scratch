cbaa3525172b5360edcd35cd57f0fa916994cc84
mohsin-ashraf <mohsin.ashraf12@gmail.com>
2020-04-11 19:53:51 +0500

    implimentation of neuron

---
+++ neuron.py
@@ -0,0 +1,7 @@
+inputs = [3.2, 2.5, 5.5]
+wights = [2.5, 1.7, 3.1]
+bias = 1
+
+output = inputs[0] * wights[0] + inputs[1] * wights[1] + inputs[2] * wights[2] + bias
+
+print ("Neuron output: {}".format(output))
